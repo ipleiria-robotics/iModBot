01c217f87faf741e4e2c8227cbebbe3d1a7b7931
Paulo Sousa <anitinha1997@gmail.com>
2022-06-21 19:05:50 +0100

    Updated AR folder
    AR folder only

--- 6_Realidade_Aumentada/iModBot-Docker/ROS2_Workspace/build/stop_oncollision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ipleiria/dev_workspace/src/stop_oncollision
-# Build directory: /home/ipleiria/dev_workspace/build/stop_oncollision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
